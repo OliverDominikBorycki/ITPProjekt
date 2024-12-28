68e721fecc121a5eab7b3c9e494188a7f2181a18
olbo <oliborycki@gmail.com>
2024-12-28 23:25:45 +0100

    Bilder werden auf GitHub aufgerufen

--- Model4B1W.java
+++ Model4B1W.java
@@ -1,21 +1,41 @@
-import java.io.File;
+import java.net.URL;
 
 public class Model4B1W {
-    private String[] words = {"Aufwandschätzung", "Meilenstein"};
-    private String[][] imagePaths = {
-            {"C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Aufwandschätzung/Aufwand.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Aufwandschätzung/bürodaten.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Aufwandschätzung/schätzung.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Aufwandschätzung\\job-5382501_1280.jpg"},
-            {"C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Meilenstein\\Meilenstein.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Meilenstein\\pokal.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Meilenstein\\meilenstein3.jpg", "C:\\Users\\olibo\\OneDrive\\Desktop\\Kleine Projekte\\ITPProjekt\\Meilenstein\\meilenstein2.jpg"}
+    private String[] words = {"Aufwandsschätzung", "Meilenstein", "Vertragsgegenstand"};
+    private String[][] imageUrls = {
+            {
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Aufwandschaetzung/Aufwand.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Aufwandschaetzung/buerodaten.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Aufwandschaetzung/zeitschaetzung.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Aufwandschaetzung/schaetzung.jpg?raw=true"
+            },
+            {
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Meilenstein/Meilenstein.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Meilenstein/meilenstein2.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Meilenstein/pokal.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Meilenstein/meilenstein3.jpg?raw=true"
+            },
+            {
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Vertragsgegenstand/genehmigt.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Vertragsgegenstand/handshake.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Vertragsgegenstand/vertrag2.jpg?raw=true",
+                    "https://github.com/OliverDominikBorycki/BilderRepository/blob/main/Vertragsgegenstand/vertrag.jpg?raw=true"
+            }
     };
     private int currentIndex = 0;
     private int score = 0;
 
-    public File[] getCurrentImages() {
-        String[] paths = imagePaths[currentIndex];
-        File[] files = new File[paths.length];
-        for (int i = 0; i < paths.length; i++) {
-            files[i] = new File(paths[i]);
+    public URL[] getCurrentImages() {
+        String[] urls = imageUrls[currentIndex];
+        URL[] urlObjects = new URL[urls.length];
+        try {
+            for (int i = 0; i < urls.length; i++) {
+                urlObjects[i] = new URL(urls[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return files;
+        return urlObjects;
     }
 
     public String getCurrentWord() {
